150e4405cc717b1a0f678d47ae2667e1fca24ac0
Christian Kniep <christian.kniep@memverge.com>
2025-09-18 10:15:54 -0700

    add Google OAUTH config

--- start-stack-ec2.sh
+++ start-stack-ec2.sh
@@ -7,6 +7,13 @@ cd /tmp
 
 OPENAI_API_KEY=$(aws secretsmanager get-secret-value   --secret-id memmachine/openai-key --region us-east-1 --query SecretString   --output text)
 echo "OPENAI_API_KEY=$OPENAI_API_KEY" >> .env
+echo "WEBUI_URL=https://playground.memmachine.ai" >> .env
+echo "GOOGLE_REDIRECT_URI=https://playground.memmachine.ai/oauth/google/callback" >> .env
+GOOGLE_CLIENT_ID=$(aws secretsmanager get-secret-value   --secret-id memmachine/google/client_id --region us-east-1 --query SecretString   --output text)
+echo "GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID" >> .env
+GOOGLE_CLIENT_SECRET=$(aws secretsmanager get-secret-value   --secret-id memmachine/google/client_secret --region us-east-1 --query SecretString   --output text)
+echo "GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET" >> .env
+
 
 aws s3 cp s3://my-memmachine-configs/docker-compose.yml .
 aws s3 cp s3://my-memmachine-configs/configuration.yml .
